b2204111643d8821fb9e02d6e1263569c87a52d7
sachin <sv19158161@gmail.com>
2022-08-22 16:15:03 +0530

    array with pushmethod

--- module2/array.js
+++ module2/array.js
@@ -3,5 +3,18 @@ let arr=[];
 console.log(arr);
 
 //array with element
-let earr=[1,2,4,5,"Hello i am string ",false,'c',4.5];
+let earr=[1,2,3,4,5,"Hello i am string ",false,'c',4.5];
 console.log(earr);
+
+//accessing array element
+console.log(earr[5]);
+console.log(earr[6]);
+//you can replace the element
+earr[4]='nothing';
+earr[6]='wrong';
+console.log(earr);
+
+//array method 
+console.log("Array before push :"+earr);
+earr.push("new item");
+console.log("Array after push : "+earr);
