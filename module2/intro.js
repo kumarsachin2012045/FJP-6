3082cabdcb9b9f7935c7417cb4471fe368498821
sachin <sv19158161@gmail.com>
2022-08-22 14:41:42 +0530

    intro to js

---
+++ module2/intro.js
@@ -0,0 +1,36 @@
+//how to print in js
+console.log("swagat hai in javascript");
+//how to declare in js
+let a=10;
+console.log(a);
+let ch='c';
+console.log(ch)
+let str='i am string';
+console.log(str);
+let bool=true;
+console.log(bool);
+//loops
+for(let i=1;i<5;i++)
+console.log(i);
+//while loops
+let i=5;
+while(i>0)
+{
+    console.log(i);
+    i--;
+}
+//is prime in js
+let n=13;
+let k=0;
+for(let i=2;i*i<n;i++)
+{
+    if(n%i==0)
+    {
+        k=1;
+        break;
+    }
+}
+if(k==0)
+  console.log("number is not prime");
+else
+  console.log("number is not prime");
