b8d54651545e2809102fbdbf766e448f8e665ab9
sachin <sv19158161@gmail.com>
2022-08-22 17:36:18 +0530

    array method

--- module2/array.js
+++ module2/array.js
@@ -15,6 +15,23 @@ earr[6]='wrong';
 console.log(earr);
 
 //array method 
+console.log("Array method :::::::::::::::::::::::::::::::")
+//1.push//add in last
 console.log("Array before push :"+earr);
 earr.push("new item");
 console.log("Array after push : "+earr);
+//2.pop//remove form last
+console.log("Array before pop :"+earr);
+earr.pop();
+console.log("Array after pop : "+earr);
+//4.unshift//it add in staring and it take parameter
+console.log("Array before unshift :"+earr);
+earr.unshift("add in starting");
+console.log("Array after unshift : "+earr);
+//3.shift //it remove from strting
+console.log("Array before shift :"+earr);
+earr.shift();
+console.log("Array after shift : "+earr);
+//length of array
+let len=earr.length;
+console.log("length of array : "+len);
