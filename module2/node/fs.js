31531933d700f1c4c30d3870b9068eb0a77d4e94
sachin <sv19158161@gmail.com>
2022-08-23 21:17:49 +0530

    create and read folder

--- module2/node/fs.js
+++ module2/node/fs.js
@@ -1,4 +1,3 @@
-const { Console } = require('console');
 let fs=require('fs');
 //console.log(fs);
 let path=require('path');//yaha path ki need hai isliye require karna padega
@@ -34,12 +33,31 @@ console.log("content after update : ");
 fs.appendFileSync(filePath," Newly added content");
 let content3=fs.readFileSync(filePath);
 console.log(""+content3);
-
-
-
-
-
 //D- delete//isme file ka path de do bas us file ko hta dega
 
 fs.unlinkSync(filePath);
 //delete wale ko comment karke dekho ge to file dikh jayega jo create hua tha
+
+
+
+//task hai how to create directory google se puch lo 
+//aur ye bhi puchh lo folder pahle se exist kar rahi ya nhi kaise check kare
+
+//C-Create a directory //agr phle se hoga directory to error dega nhi hoga to bna dega
+//fs.mkdirSync("Hamaridirectory");//jab file bnaye the to path diya tha yha kyo nhi diya kyoki 
+//mkdirSync use karne par ye same folder/directory me folder/directory bna dega 
+//agar path doge to us path par jakar bna dega directory path nhi doge to kewal name doge to usi folder me bna dega
+//how to check already directory exist or not condition lga do taki error na aye search kar lo google se
+//sab tareeka google se search kar ke nikal lo yad nhi karna hai chahe syntax ho ya jo kuch bhi ho
+if(!fs.existsSync("Hamaridirectory"))
+   fs.mkdirSync("Hamaridirectory");
+
+   //R-read a directory
+let folderPath="C:\\Users\\kumar\\OneDrive\\Desktop\\FJP-6\\module1";
+//module1 ka path diya
+let contentOfFolder=fs.readdirSync(folderPath);
+console.log(contentOfFolder);//read karke array me dega sare file ka
+
+//JAB COPY PATH KARKE folderPath ME Rakhoge single slace se seprated hoga  to run karoge to error dega
+//error na aye isliye double slace (\\) se replace kar dena
+
