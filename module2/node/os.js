a93f2581dfba073a4e13196b63de538e59613a90
sachin <sv19158161@gmail.com>
2022-08-23 08:41:11 +0530

    os

--- module2/node/os.js
+++ module2/node/os.js
@@ -1,5 +1,5 @@
-let os=require('os');//it also a object
-console.log(os);
+let os=require('os');//it also a object//it contain more key as a function so for access we use call 
+console.log(os);//kyo ki object hai to dot ke sath access karenge
 console.log(os.hostname());
 console.log(os.networkInterfaces());
 console.log(os.userInfo());
