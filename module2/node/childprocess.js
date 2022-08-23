057a627f03603e916613d55edec6bfa0ab212cbf
sachin <sv19158161@gmail.com>
2022-08-23 08:36:48 +0530

    os

---
+++ module2/node/childprocess.js
@@ -0,0 +1,4 @@
+let cp=require('child_process');//it is a object//iski help se dusri file ko run kar sakte hai  
+//console.log(cp);
+//open calculator with the help of object containing key
+cp.execFileSync("calci_calculator");
---
+++ module2/node/os.js
@@ -0,0 +1,5 @@
+let os=require('os');//it also a object
+console.log(os);
+console.log(os.hostname());
+console.log(os.networkInterfaces());
+console.log(os.userInfo());
