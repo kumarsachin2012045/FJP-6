b8c73167ef83f82d869586a063ebd6070c3ab795
sachin <sv19158161@gmail.com>
2022-08-24 18:54:25 +0530

    q2

--- module2/node/question/q2.js
+++ module2/node/question/q2.js
@@ -3,7 +3,10 @@
 //put in these folder in organise folder
 let fs=require('fs');
 let path=require('path');
-let arr=['Audio','Video','Software','Document','Application','Other'];
+let arr=['Audio','Video','Software','Document','Application','Other'];//ye sare folder
+let nameArr=['abc','efd','xyz','def'];//4 filo ke name 
+let extArr=['.mp3','.mp4','.exe','.pdf','.txt','.zip'];
+
 //pahle organise name ka folder bnate hai question folder me
 let organisePath=path.join(__dirname,"organise");
 if(!fs.existsSync(organisePath))
@@ -13,6 +16,12 @@ for( let i=0;i<arr.length;i++){
     let folderKaPath=path.join(organisePath,arr[i]);
       if(!fs.existsSync(folderKaPath))
         fs.mkdirSync(folderKaPath);
+    for(let j=0;j<nameArr.length;j++)
+    {
+      let fileName=nameArr[j]+extArr[i];
+      let fileKaPath=path.join(folderKaPath,fileName);
+      fs.writeFileSync(fileKaPath," ");
+    }
 }
          
           
