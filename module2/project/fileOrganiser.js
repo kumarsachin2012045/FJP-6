61ed4ca42a2dd8b10365653f5fbe2e36315ef6db
sachin <sv19158161@gmail.com>
2022-08-25 08:16:48 +0530

    input and read

--- module2/project/fileOrganiser.js
+++ module2/project/fileOrganiser.js
@@ -1,12 +1,15 @@
-//task first how to take input
-// let inputArr=process.argv;
-// console.log(inputArr);//ye run akrne par ek array milega jiske 0th index par wo address hai jaha node install hai system me
-//1st index par file ka path/address milega jisme ho
-//ab input lete hai terminal par node fileOrganiser.js ke bad terminal me to wo usi array me 2nd index par milega
-//ie jo input loge wo 2nd index se milega
-                      //output
-// ['pathTonode','pathTofile','input_jo_loge]//0th index node ka address and 1st index file ka address dega 2nd index inut btayega
-let inputArr=process.argv;
-console.log(inputArr);
-let input=inputArr[2];
-console.log(input);
+//task 1 take input and assumption koi folder nhi hona chahiye folder ke andar
+let folderPath=process.argv[2];
+//console.log(folderPath);
+//task 2 read the folder
+let fs=require('fs');
+let folderExists=fs.existsSync(folderPath);
+
+if(folderExists){
+    //we will code
+    //console.log("Path is valid!!!!!!!!!!!!!!!!!!!!!!");
+    let files = fs.readdirSync(folderPath);
+    console.log(files);
+}
+else
+    console.log("Please enter valid path!!!!!!!!!!!!");
