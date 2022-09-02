2b53bad8c016a4dd337c27b12ee963a0e0033a1d
sachin <sv19158161@gmail.com>
2022-09-02 07:26:03 +0530

    Batsman name with dob

--- module2/web_scraping/batsmanDetails.js
+++ module2/web_scraping/batsmanDetails.js
@@ -17,8 +17,24 @@ function cb(error, response, html) {
             //console.log(Batsman[i].textContent);
             let batmanLink=Batsman[i].href;
             //console.log(batmanLink);//yaha se half link milega jo href me hai baki half link next page ke url me hai use add kardo to next page ki complete link milega 
-           let comleteBatsmanLink="https://www.espncricinfo.com"+batmanLink;
-           console.log(comleteBatsmanLink);
+           let comleteBatsmanLink="https://www.espncricinfo.com"+batmanLink;//link ek bar url se match kar lena jo extra ho use hta dena
+           //console.log(comleteBatsmanLink);//hme phir se naye page ki link mili ab phir se request karni padegi
+           request(comleteBatsmanLink,cb2)
         }
        }
+}
+function cb2(error,response,html){
+    if(error)
+    console.log(error);
+    else{
+        const dom = new JSDOM(html);
+        const document = dom.window.document;//hm next page par hai
+        let playerDetails=document.querySelectorAll(".ds-text-title-s.ds-font-bold.ds-text-ui-typo");
+        let player=document.querySelectorAll("h5");
+        
+            let Name=player[0].textContent;
+            let DOB=player[1].textContent;
+            console.log("Name of plyer "+Name+"Date of birth "+DOB);
+            
+    }
 }
