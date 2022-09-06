7c9b23a22e62170264b8a872fed950759084c023
sachin <sv19158161@gmail.com>
2022-09-06 23:42:12 +0530

    asynchronous concept

--- module2/web_scraping/leaderbord.js
+++ module2/web_scraping/leaderbord.js
@@ -3,7 +3,7 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const link="https://www.espn.com/cricket/scores/series/8634/womens-t20-world-cup"//https://www.espn.com ye domain name hai href me add ho ke complete link banega new page ka
 let leaderbord=[];
-request(link,cb);
+request(link,cb);//ye Asynchronous function hai
 function cb(error,response,html){
     if(error)
      console.log(error);
@@ -34,15 +34,16 @@ function cb2(error,response,html){
                 let runs=cell[2].textContent;
                 let balls=cell[3].textContent;
                 let fours=cell[4].textContent;
-                let sixs=cell[5].textContent;
-                //console.log("Nmae : ",name," Runs : ",runs," Balls : ",balls," Four : ",fours," Sixes : ",sixs);
+                let sixes=cell[5].textContent;
+                //console.log("Nmae : ",name," Runs : ",runs," Balls : ",balls," Four : ",fours," Sixes : ",sixes);
+                processPlayer(name,runs,balls,fours,sixes)
             }
         }
     }
 }
-processPlayer('Muneeba Ali','10','13','2','0')//string kyo pass kiya .textContent likhte hai to string milta hai isse add karne par concatenate ho jata hai
-processPlayer('Muneeba Ali','25','26','3','0')//Number ki helps sabko integer me change kar diya
-console.log(leaderbord);
+// processPlayer('Muneeba Ali','10','13','2','0')//string kyo pass kiya .textContent likhte hai to string milta hai isse add karne par concatenate ho jata hai
+// processPlayer('Muneeba Ali','25','26','3','0')//Number ki helps sabko integer me change kar diya
+// console.log(leaderbord);
 function processPlayer(name,runs,balls,fours,sixes){
     runs=Number(runs);
     balls=Number(balls);//conversion from string to integer
@@ -70,4 +71,7 @@ function processPlayer(name,runs,balls,fours,sixes){
         Sixes:sixes
      };
      leaderbord.push(obj);
- }
+ }//leader bord to ban gya hai
+console.log(leaderbord);//iska output empty[] kyo ki callback(cb)abhi chla hi nhi ye abhi node API par rkha hai
+//jab Asynchronous function apna kam kar lega tab callback function ko waiting/task queue me bhejega
+//aur tab ye call stack me jake execute hoga iska matlab leaderbord ko yaha nhi likh skte
